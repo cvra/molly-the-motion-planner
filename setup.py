70674c15f83568bf972b39d839b90c8237021edc
31415us <pius.vondaeniken@epfl.ch>
2015-04-06 15:32:00 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python
+
+from setuptools import setup
+
+args = dict(
+        name='molly',
+        version='0.1',
+        description='simple path planner',
+        packages=['molly'],
+        install_requires=['heapdict'],
+        author='Pius von Däniken'
+        url='https://github.com/cvra/molly-the-motion-planner'
+)
+
+setup(**args)
